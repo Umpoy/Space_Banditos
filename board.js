475a0a3a3c095fde93b707ef2616719018362707
ericyoon1 <eriqyoon@gmail.com>
2017-09-27 16:17:10 -0700

    tried win condition

--- board.js
+++ board.js
@@ -20,24 +20,71 @@ function makeBoard(){
 		}
 	}
     $(".square").on("click", printInSegment);
+    //$(".square").on("click", checkGameOver);
 }
 
 function printInSegment()
-       {
-           console.log('zxc');
-           if (counter % 2 === 0) {
-                $(this).addClass('morty');
+	{
+		console.log('zxc');
+		if (counter % 2 === 0) {
+			$(this).addClass('morty');
+			// console.log('X');
+			//$(this).text('X');
+		} else {
+			$(this).addClass('rick');
+			// //console.log('O');
+			//$(this).text('O');
+		}
+		counter++;
+	}
 
-               // console.log('X');
-               //$(this).text('X');
-           } else {
-               $(this).addClass('rick');
+        function checkGameOver()
+        {
+            console.log("checking winning condition");
+            for (var i=0; i<tableSize; i++)
+            {
+                for (var j=0; j<tableSize; j++)
+                {
 
-               // //console.log('O');
-               //$(this).text('O');
-           }
-           counter++;
-       }
+                    if ($(".square[data-row="+i+"][data-column="+j+"] ").text() !== "")
+                    {
+
+                        if ($("#ij").text() === $("#i"+"j+1").text() !== ""){
+
+                        }
+
+                        //checking row win possibilities
+                        if ($(".square[data-row="+i+"][data-column="+j+"] ").text() === $(".square[data-row="+i+"][data-column="+(j+1)+"] ").text()
+                        && $(".square[data-row="+i+"][data-column="+j+"] ").text() === $(".square[data-row="+i+"][data-column="+(j+2)+"] ").text())
+                        {
+                            console.log("win")
+                        }
+
+                        //checking column win possibilities
+                        if ($(".square[data-row="+i+"][data-column="+j+"] ").text() === $(".square[data-row="+(i+1)+"][data-column="+j+"] ").text()
+                            && $(".square[data-row="+i+"][data-column="+j+"] ").text()===$(".square[data-row="+(i+2)+"][data-column="+j+"] ").text())
+                        {
+                            console.log("win")
+                        }
+
+                        //checking diagonal win possibilities
+                        if ($(".square[data-row="+i+"][data-column="+j+"] ").text() === $(".square[data-row="+(i+1)+"][data-column="+(j+1)+"] ").text()
+                            && $(".square[data-row="+i+"][data-column="+j+"] ").text()===$(".square[data-row="+(i+2)+"][data-column="+(j+2)+"] ").text())
+                        {
+                            console.log("win")
+                        }
+
+                        //checking diagonal win possibilities
+                        if ($(".square[data-row="+i+"][data-column="+j+"] ").text() === $(".square[data-row="+(i+1)+"][data-column="+(j-1)+"] ").text()
+                            && $(".square[data-row="+i+"][data-column="+j+"] ").text()===$(".square[data-row="+(i+2)+"][data-column="+(j-2)+"] ").text())
+                        {
+                            console.log("win")
+                        }
+
+                    }
+                }
+            }
+        }
 
 // // Get the modal
 // var modal = document.getElementById('myModal');
